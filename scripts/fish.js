6ea8499ec85a33e58dc49e9a2a439c0eb547131c
Chris Abbondandelo <abbondcj@gmail.com>
2022-01-18 20:07:25 -0600

    Got the html to display correctly, css still needs work

--- scripts/fish.js
+++ scripts/fish.js
@@ -1,18 +1,30 @@
-import { getFish } from "./database.js";
+import { database } from "./database.js";
 
-export const fishList = () => {
-    let fishes = getFish()
 
-    for (let fish of fishes) {
-        htmlString += `<div class="fish">
-        <img src="${fish.image}" alt="${fish.name}">
-        <h3>${fish.name}</h3>
-        <h2>${fish.species}</h2>
-        <p>${fish.length}</p>
-        <p>${fish.harvested}</p>
-        <p>${fish.diet}</p>
-    </div>`
-    }
 
-    return htmlString
+const populate = () => {
+
+    let displayFish = document.getElementById("fishesContainer");
+    let newArray = database.fish.map(fish => {
+        return fish;
+    });
+
+    let htmlString = `<div class="fishListContainer">`;
+
+    for (const fishes of newArray) {
+        htmlString+=`
+        <div class="fishCard">
+            <img src="${fishes.image}" alt="${fishes.name}">
+            <h3>${fishes.name}</h3>
+            <p>Species: ${fishes.species}</p>
+            <p>Length: ${fishes.length}</p>
+            <p>Harvested: ${fishes.harvested}</p>
+            <p>Diet: ${fishes.diet}</p>
+        </div>`
+    };
+
+    htmlString+=`</div>`;
+    displayFish.innerHTML=htmlString;
 };
+
+populate();
